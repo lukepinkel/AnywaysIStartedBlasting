3d2bcd7c6379329862a9e4f19888485d5df0a139
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2023-10-18 20:41:13 -0700

    Update Posets_Trimmed.thy

--- Posets_Trimmed.thy
+++ Posets_Trimmed.thy
@@ -311,6 +311,9 @@ definition is_closure_range::"'X::order set \<Rightarrow> 'X::order set \<Righta
 definition closure_from_closure_range::"'X::order set \<Rightarrow> 'X::order set \<Rightarrow> ('X::order \<Rightarrow> 'X::order)"
   where "closure_from_closure_range C X = (\<lambda>x. Minimum (PrincipalFilter x C))"
 
+definition fixed_point_set::"('X::order \<Rightarrow> 'X::order) \<Rightarrow> 'X::order set \<Rightarrow> 'X::order set" 
+  where "fixed_point_set f X = {x \<in> X. f x = x}"
+
 lemma closure_equiv:
   "is_a_closure f X \<longleftrightarrow> (hull_equivalence f X) \<and> (is_ftriple f X X)"
 proof-
@@ -367,5 +370,140 @@ proof-
     by (smt (verit, ccfv_threshold) CollectD PowD PrincipalFilter_def assms closure_from_closure_range_def in_mono is_a_closure_def is_closure_range_def is_ftriple_def min_lemma2)
 qed
 
+
+definition pointwise_lt:: "('X::order \<Rightarrow> 'Y::order) \<Rightarrow> 
+                           ('X::order \<Rightarrow> 'Y::order) \<Rightarrow>
+                           bool"
+                           (infix "\<preceq>" 50) where
+   "f \<preceq> g  \<longleftrightarrow> (\<forall>x. (f x \<le> g x ))"
+
+definition precedes_on_dom:: "('X::order  \<Rightarrow> 'Y::order ) \<Rightarrow> 
+                              ('X::order  \<Rightarrow> 'Y::order ) \<Rightarrow>
+                               'X::order set  \<Rightarrow>  bool"
+where "precedes_on_dom f g X \<equiv> (\<forall>x \<in> X. (f x \<le> g x))"
+
+
+lemma cliso_lemma1:
+  assumes P0:"is_a_closure f X" and
+          P1:"is_a_closure g X" and
+          P2:"f \<preceq> g"
+  shows "{x \<in> X. g x \<le> x} \<subseteq> {x \<in> X. f x \<le> x}"
+proof-
+  let ?Eg="{x \<in> X. g x \<le> x}" and ?Ef="{x \<in> X. f x \<le> x}"
+  have B0:"f \<preceq> g \<longrightarrow> (\<forall>x \<in> X. g x \<le> x \<longrightarrow> f x \<le> x)" by (meson dual_order.trans pointwise_lt_def) 
+  have B1:"f \<preceq> g \<longrightarrow> (\<forall>x \<in> X. x \<in>?Eg \<longrightarrow> x \<in> ?Ef) " using B0 by blast
+  have B2:"f \<preceq> g \<longrightarrow> ?Eg \<subseteq> ?Ef"  using B1 by blast
+  with B2 show ?thesis using P2 by fastforce 
+qed
+
+lemma lem0:
+  assumes P0:"A \<subseteq> fixed_point_set f X" and
+          P1:"is_a_closure f X" and
+          P2:"HasInf A X" 
+  shows "f (Infimum A X) = Infimum A X"
+proof-
+  let ?i="Infimum A X"
+  have A0:"?i \<le> f ?i"
+    by (metis (mono_tags, lifting) HasInf_def Infimum_def LowerBounds_def P1 P2 is_a_closure_def is_extensive_def max_lemma2 mem_Collect_eq)
+  have A1:"(\<forall>a \<in> A.  ?i \<le> a) \<longrightarrow> (\<forall>a \<in> A. f ?i \<le> f a)\<longrightarrow> (\<forall>a \<in> A. f ?i \<le> a) "
+    by (metis (mono_tags, lifting) CollectD P0 fixed_point_set_def in_mono)
+  have A2:"f ?i \<le> ?i"
+    by (smt (verit, del_insts) HasInf_def Infimum_def LowerBounds_def P0 P1 P2 fixed_point_set_def is_a_closure_def is_ftriple_def is_isotone_def max_lemma2 mem_Collect_eq subset_eq)
+  from A0 A2 have A3:"f ?i = ?i"
+    using order_antisym by blast
+  with A3 show ?thesis by simp
+qed
+
+lemma lem1:
+  assumes P0: "is_a_closure f X"
+  shows "is_closure_range (f`X) X"
+proof-
+  let ?C="f`X"
+  have A0: "\<forall>x \<in> X. (x \<le> f x)"  using assms is_a_closure_def is_extensive_def by blast
+  have A1:"\<forall>x \<in> X. (\<exists>c \<in> ?C . (x \<le> c))" using A0 by blast
+  have A2:"\<forall>x \<in> X. (PrincipalFilter x ?C \<noteq> {}) " using A0 A1 PrincipalFilter_def by blast
+  have A3:"\<forall>x \<in> X. (\<forall>y \<in> (PrincipalFilter x ?C). (x\<le> y))" by (simp add: PrincipalFilter_def)
+  have A4:"\<forall>x \<in> X. (\<forall>y \<in> (PrincipalFilter x ?C). (f x \<le> f y))"
+    by (metis (mono_tags, lifting) CollectD PrincipalFilter_def assms imageE is_a_closure_def is_ftriple_def is_isotone_def) 
+  have A5:"\<forall>x \<in> X. (\<forall>y \<in> (PrincipalFilter x ?C). (f x \<le> y))"
+    by (metis (no_types, lifting) A4 CollectD PrincipalFilter_def assms image_iff is_a_closure_def is_idempotent_def)
+  have A6:"\<forall>x \<in> X. (HasMinimum (PrincipalFilter x ?C))"
+    using A0 A5 HasMinimum_def PrincipalFilter_def by fastforce
+  have A7:"?C \<in> Pow X"
+    by (meson Pow_top assms is_a_closure_def lemf1)
+  have A8:"is_closure_range ?C  X"
+    by (meson A6 A7 is_closure_range_def)
+  with A8 show ?thesis by simp
+qed
+
+lemma lem2:
+  assumes P0:"is_a_closure f X"
+  shows "\<forall>x \<in> X. (f x = (closure_from_closure_range (f`X) X) x)"  
+proof-
+  let ?C="f`X"  
+  let ?G="\<lambda>x. (closure_from_closure_range ?C X)(x)"
+  have A0:"\<forall>x \<in> X. (f x =?G x )"
+    by (simp add: PrincipalFilter_def assms closure_from_closure_range_def smallest_larger_closed_element)
+  with A0 show ?thesis by simp
+qed
+
+
+lemma lem3:
+  assumes P0:"is_closure_range C X"
+  shows "(closure_from_closure_range C X)`(X) = C"  
+proof-
+  let ?f="closure_from_closure_range C X"  
+  have A0:"?f`(X) = {y \<in> X. \<exists>x \<in> X. ?f x = y}"
+    using assms closure_equiv cr_cl_is_closure is_ftriple_def by fastforce
+  have A1:"?f`(X) = {y \<in> X. \<exists>x \<in> X. y= Minimum(PrincipalFilter x C)}"
+    by (smt (verit, best) A0 Collect_cong closure_from_closure_range_def)
+  have A2:"?f`(X) \<subseteq> C"
+    by (metis (no_types, lifting) PrincipalFilter_def assms closure_from_closure_range_def image_subsetI is_closure_range_def mem_Collect_eq min_lemma2)
+  have A3:"\<forall>c \<in> C. (c = Minimum(PrincipalFilter c C))"
+    by (smt (verit, ccfv_threshold) Pow_def PrincipalFilter_def assms insert_subset is_closure_range_def mem_Collect_eq min_lemma2 order_antisym order_refl)
+  have A4:"C \<subseteq> ?f`(X)"
+    by (smt (verit) A3 PowD assms closure_from_closure_range_def image_iff is_closure_range_def subsetD subsetI)
+  have A5:"C= ?f`(X)"
+    by (simp add: A2 A4 subset_antisym)
+  with A5 show ?thesis by simp
+qed
+
+lemma lem4:
+  assumes P0:"is_a_closure f1 X" and
+          P1:"is_a_closure f2 X"
+  shows "(precedes_on_dom f1 f2  X) \<longleftrightarrow> ((f2`X) \<subseteq> (f1`X))"
+proof-
+  let ?C1="f1`X" and ?C2="f2`X"
+  let ?L="(precedes_on_dom f1 f2 X)"
+  let ?R="(?C2 \<subseteq> ?C1 )"
+  have LtR:"?L \<longrightarrow>?R"
+  proof-
+    have A0:"?L \<longrightarrow> (\<forall>x \<in> X. ((PrincipalFilter x ?C2) \<subseteq> (PrincipalFilter x ?C1)))"
+      by (smt (verit) P0 P1 PrincipalFilter_def image_iff is_a_closure_def is_extensive_def is_ftriple_def is_idempotent_def mem_Collect_eq order_antisym_conv precedes_on_dom_def subsetI)
+    have A1:"?L \<longrightarrow> (\<forall>x \<in> X. (Minimum(PrincipalFilter x ?C1) \<le>  Minimum(PrincipalFilter x ?C2) ))"
+      by (meson A0 P0 P1 in_mono is_closure_range_def lem1 min_lemma2)
+    have A2:"?L \<longrightarrow> (?C2 \<subseteq> ?C1)"
+      by (smt (verit, ccfv_threshold) P0 P1 dual_order.antisym image_iff image_subset_iff is_a_closure_def is_extensive_def is_ftriple_def is_idempotent_def precedes_on_dom_def) 
+    with A2 show ?thesis by simp
+  qed
+  have RtL:"?R \<longrightarrow> ?L"
+  proof-
+    let ?u="\<lambda>x. \<lambda>C. (PrincipalFilter x C) "
+    have B0:"\<forall>x \<in> X. (?R \<longrightarrow> (?u x ?C2 \<subseteq> (?u x ?C1)))"
+      by (simp add: Collect_mono_iff PrincipalFilter_def in_mono)
+    have B1:"\<forall>x \<in> X.  ?R \<longrightarrow> (Minimum(?u x ?C1) \<le>  Minimum(?u x ?C2))"
+      by (meson B0 P0 P1 in_mono is_closure_range_def lem1 min_lemma2)
+    have B2:"\<forall>x \<in> X. (f1 x  = Minimum(?u x ?C1))"
+      by (simp add: P0 PrincipalFilter_def smallest_larger_closed_element)
+    have B3:"\<forall>x \<in> X. (f2 x  = Minimum(?u x ?C2))"
+      by (simp add: P1 PrincipalFilter_def smallest_larger_closed_element)
+    have B4: "?R\<longrightarrow>(\<forall>x \<in> X. f1 x  \<le>  f2 x)"
+      using B1 B2 B3 by fastforce
+    have B5: "?R\<longrightarrow>?L"  by (simp add: B4 precedes_on_dom_def)
+    with B5 show ?thesis by simp
+  qed
+  from LtR RtL have LtRtL:"?L \<longleftrightarrow> ?R" by blast
+  with LtRtL show ?thesis by simp
+qed
 
 end
