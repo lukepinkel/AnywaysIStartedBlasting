25f21278115bdd3727b49d2d9bd24ca7631d4858
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2023-12-11 12:40:46 -0800

    Update Prod.thy

--- Prod.thy
+++ Prod.thy
@@ -14,6 +14,53 @@ lemma prod_mem:
   "f \<in> Prod I X \<longleftrightarrow> (\<forall>i \<in> I. (f i) \<in> (X i))"
   by (simp add: CartesianProduct_def)
 
+
+lemma axiom_of_choice_lol:
+  fixes I::"'I set"
+  fixes X::"'I \<Rightarrow> 'X set"
+  assumes A0:"I \<noteq> {}" and A1:"\<forall>i \<in> I. X i \<noteq> {}"
+  shows "Prod I X \<noteq> {}"
+proof-
+  define f where "f=(\<lambda>i. SOME x. (x \<in> (X i)))"
+  have "f \<in> Prod I X"
+    by (metis A1 equals0I f_def prod_mem verit_sko_ex')
+  show ?thesis
+    using \<open>(f::'I \<Rightarrow> 'X) \<in> Prod.Prod (I::'I set) (X::'I \<Rightarrow> 'X set)\<close> by auto
+qed
+
+lemma projection_is_surjective:
+  fixes I::"'I set"
+  fixes X::"'I \<Rightarrow> 'X set"
+  assumes A0:"I \<noteq> {}" and A1:"\<forall>i \<in> I. X i \<noteq> {}" 
+  shows "\<forall>i \<in> I. \<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
+proof-
+  have B0:"Prod I X \<noteq> {}"
+    by (simp add: A0 A1 axiom_of_choice_lol)
+  have "\<forall>i \<in> I. \<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
+  proof
+    fix i assume "i \<in> I"
+    show "\<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
+    proof
+      fix x assume "x \<in> (X i)"
+      obtain f0 where "f0 \<in> Prod I X"
+        using B0 by blast
+      define f where "f=(\<lambda>j. if j=i then x else (f0 j))"
+      have "f \<in> Prod I X"
+        by (metis \<open>(f0::'I \<Rightarrow> 'X) \<in> Prod.Prod (I::'I set) (X::'I \<Rightarrow> 'X set)\<close> \<open>(x::'X) \<in> (X::'I \<Rightarrow> 'X set) (i::'I)\<close> f_def prod_mem)
+      have "f i = x"
+        by (simp add: f_def)
+      show "\<exists>f \<in> (Prod I X). (f i) = x"
+        using \<open>(f::'I \<Rightarrow> 'X) (i::'I) = (x::'X)\<close> \<open>(f::'I \<Rightarrow> 'X) \<in> Prod.Prod (I::'I set) (X::'I \<Rightarrow> 'X set)\<close> by auto
+      qed
+    qed
+  show ?thesis
+    by (simp add: \<open>\<forall>i::'I\<in>I::'I set. \<forall>x::'X\<in>(X::'I \<Rightarrow> 'X set) i. \<exists>f::'I \<Rightarrow> 'X\<in>Prod.Prod I X. f i = x\<close>)
+qed
+
+lemma comp_inverse:
+  "UNIV-(UNIV-X) = X"
+  by (simp add: Diff_Diff_Int)
+
 lemma union_of_intersections:
   fixes I::"'I set"
   fixes J::"'I \<Rightarrow> 'J set"
@@ -72,9 +119,6 @@ proof-
 qed
    
 
-lemma comp_inverse:
-  "UNIV-(UNIV-X) = X"
-  by (simp add: Diff_Diff_Int)
 
 lemma intersection_of_unions:
   fixes I::"'I set"
@@ -161,5 +205,63 @@ proof-
     using A2 A3 LR RL by fastforce
 qed      
   
+
+lemma cartesian_product_intersection_union:
+  fixes I::"'I set"
+  fixes J::"'I \<Rightarrow> 'J set"
+  fixes X::"'I \<Rightarrow> 'J \<Rightarrow> 'X set"
+  assumes A0:"I \<noteq> {}" and A1:"\<forall>i \<in> I. J i \<noteq> {}"
+  shows "Prod I (\<lambda>i. (\<Inter>j \<in> (J i). (X i j))) = (\<Inter>f \<in> (Prod I J). (Prod I (\<lambda>i. (X i (f i)))))"
+proof-
+  define G where A2:"G=(\<lambda>i. (\<Inter>j \<in> (J i). (X i j)))"
+  define F where A3:"F=(\<lambda>f i. (X i (f i)))"
+  let ?PIJ="Prod I J"
+  let ?L="Prod I G"
+  let ?R="(\<Inter>f \<in>?PIJ. (Prod I (F f)))"
+  have B0:"\<forall>g. g \<in> ?L \<longleftrightarrow> (\<forall>i \<in> I. \<forall>j \<in> (J i). (g i) \<in> (X i j))"
+    by (simp add: \<open>G::'I \<Rightarrow> 'X set \<equiv> \<lambda>i::'I. \<Inter> ((X::'I \<Rightarrow> 'J \<Rightarrow> 'X set) i ` (J::'I \<Rightarrow> 'J set) i)\<close> prod_mem)
+  have B1:"\<forall>g. g \<in> ?R \<longleftrightarrow> (\<forall>f \<in> ?PIJ. \<forall>i \<in> I. (g i) \<in> (X i (f i)))"
+    by (simp add: A3 prod_mem)
+  have B2:"\<forall>f. f \<in> ?PIJ \<longleftrightarrow> (\<forall>i \<in> I. (f i) \<in> (J i))"
+    by (simp add: prod_mem)
+  have LR:"?L \<subseteq> ?R"
+    proof-
+      have LR0:"\<And>g. g \<in> ?L \<longrightarrow> g \<in> ?R"
+        proof
+          fix g assume LR0A0:"g \<in> ?L"
+          show "g \<in> ?R"
+            by (metis B0 B1 LR0A0 prod_mem)
+        qed
+      show ?thesis
+        using LR0 by blast
+   qed
+  have RL:"?R \<subseteq> ?L"
+   proof-
+      have RL0:"\<And>g. g \<in> ?R \<longrightarrow> g \<in> ?L"
+        proof
+          fix g assume A5:"g \<in> ?R"
+          have RL0B0:"\<forall>f. f \<in> ?PIJ \<longrightarrow> (\<forall>i\<in>I. (g i) \<in> (X i (f i)))"
+            by (meson A5 B1)
+          have RL0B1:"\<forall>f \<in> ?PIJ. (\<forall>i \<in> I. (f i) \<in> (J i))"
+            using B2 by fastforce
+          have RL0B2:"(\<forall>i \<in> I. \<forall>j \<in> (J i). (g i) \<in> (X i j))"
+          proof
+            fix k assume "k \<in> I"
+            show "\<forall>h \<in> (J k). (g k) \<in> (X k h)"
+            proof
+              fix h assume "h \<in> (J k)"
+              show "(g k) \<in> (X k h)"
+                by (metis A0 A1 RL0B0 \<open>(h::'J) \<in> (J::'I \<Rightarrow> 'J set) (k::'I)\<close> \<open>(k::'I) \<in> (I::'I set)\<close> projection_is_surjective)
+            qed
+          qed
+          show "g \<in> ?L"
+            using B0 RL0B2 by blast
+      qed
+      show ?thesis
+        using RL0 by blast
+  qed
+  show ?thesis
+    using A2 A3 LR RL by blast
+qed
 
 end
