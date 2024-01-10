e221932788629287354ca51dae71569b3e2a7531
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2024-01-10 06:27:18 -0800

    Update Prod.thy

--- Prod.thy
+++ Prod.thy
@@ -1,5 +1,5 @@
 theory Prod
-  imports Main
+  imports Main "HOL-Library.Nat_Bijection" "HOL-Library.FuncSet" "HOL-Library.Countable_Set"
 begin
 
 declare [[show_types]]
@@ -31,30 +31,31 @@ qed
 lemma projection_is_surjective:
   fixes I::"'I set"
   fixes X::"'I \<Rightarrow> 'X set"
-  assumes A0:"I \<noteq> {}" and A1:"\<forall>i \<in> I. X i \<noteq> {}" 
+  assumes A0:"I \<noteq> {}" and 
+          A1:"\<forall>i \<in> I. X i \<noteq> {}" 
   shows "\<forall>i \<in> I. \<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
 proof-
   have B0:"Prod I X \<noteq> {}"
     by (simp add: A0 A1 axiom_of_choice_lol)
-  have "\<forall>i \<in> I. \<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
+  have B1:"\<forall>i \<in> I. \<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
   proof
-    fix i assume "i \<in> I"
-    show "\<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
+    fix i assume A2:"i \<in> I"
+    show B2:"\<forall>x \<in> (X i). \<exists>f \<in> (Prod I X). (f i) = x"
     proof
-      fix x assume "x \<in> (X i)"
-      obtain f0 where "f0 \<in> Prod I X"
+      fix x assume A3:"x \<in> (X i)"
+      obtain f0 where B3:"f0 \<in> Prod I X"
         using B0 by blast
       define f where "f=(\<lambda>j. if j=i then x else (f0 j))"
-      have "f \<in> Prod I X"
-        by (metis \<open>(f0::'I \<Rightarrow> 'X) \<in> Prod.Prod (I::'I set) (X::'I \<Rightarrow> 'X set)\<close> \<open>(x::'X) \<in> (X::'I \<Rightarrow> 'X set) (i::'I)\<close> f_def prod_mem)
-      have "f i = x"
+      have B4:"f \<in> Prod I X"
+        by (metis A3 B3 f_def prod_mem)
+      have B5:"f i = x"
         by (simp add: f_def)
       show "\<exists>f \<in> (Prod I X). (f i) = x"
-        using \<open>(f::'I \<Rightarrow> 'X) (i::'I) = (x::'X)\<close> \<open>(f::'I \<Rightarrow> 'X) \<in> Prod.Prod (I::'I set) (X::'I \<Rightarrow> 'X set)\<close> by auto
+        using B4 B5 by blast
       qed
     qed
   show ?thesis
-    by (simp add: \<open>\<forall>i::'I\<in>I::'I set. \<forall>x::'X\<in>(X::'I \<Rightarrow> 'X set) i. \<exists>f::'I \<Rightarrow> 'X\<in>Prod.Prod I X. f i = x\<close>)
+    using B1 by blast
 qed
 
 lemma comp_inverse:
@@ -264,16 +265,193 @@ proof-
     using A2 A3 LR RL by blast
 qed
 
+definition is_right_inv_of_on::"('b \<Rightarrow> 'a) \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> 'b set \<Rightarrow> bool" where
+  "is_right_inv_of_on h f Y \<equiv> (\<forall>y. y \<in> Y \<longrightarrow> (f (h y) =y))"
+
+definition cardlt::"'a set \<Rightarrow> 'b set \<Rightarrow> bool" where 
+  "cardlt A B \<equiv> (\<exists>(f::('a \<Rightarrow> 'b)). inj_on f A \<and> f`A \<subseteq> B)"
+
+definition cardgt::"'a set \<Rightarrow> 'b set \<Rightarrow> bool" where 
+  "cardgt A B \<equiv> (A = {}) \<or> (\<exists>(f::('a \<Rightarrow> 'b)). f`A = B)"
+
+lemma right_inv_imp_surj:
+  assumes A0:"B \<noteq> {} \<and> A \<noteq> {}" and A1:"\<exists>h. (is_right_inv_of_on h f B) \<and> (range h \<subseteq> A)"
+  shows "\<forall>b. b \<in>  B \<longrightarrow> (\<exists>a \<in> A. f a =b)"
+  by (meson A1 is_right_inv_of_on_def range_subsetD)
+
+lemma right_surj_imp_right_inv:
+  assumes A0:"A \<noteq> {} \<and> B \<noteq> {} \<and> (\<forall>b. b \<in>  B \<longrightarrow> (\<exists>a \<in> A. f a =b))"
+  shows "\<exists>h. (is_right_inv_of_on h f B) \<and> (range h \<subseteq> A)"
+proof-
+  have B0: "\<forall>b. b \<in> B \<longrightarrow> ((vimage f {b}) \<noteq> {})"
+    using A0 by auto
+  define a0 where  "a0= (SOME a. a \<in> A)"
+  have B01:"a0 \<in> A"
+    by (simp add: a0_def assms some_in_eq)
+  define h where "h=(\<lambda>b. if b \<in> B then (SOME a. a \<in> A \<and> ((f a = b) )) else a0)"
+  have B1:"\<And>b. b \<in> B \<longrightarrow> (f (h b) =b)"
+  proof
+    fix b assume A2:"b \<in> B" 
+    show "(f (h b) =b)"
+      by (smt (verit, best) A2 assms h_def verit_sko_ex')
+  qed
+  have B2:"(is_right_inv_of_on h f B)"
+    by (simp add: B1 is_right_inv_of_on_def)
+  have B3:"range h \<subseteq> A"
+  proof
+    fix x assume A3:"x \<in> range h"
+    obtain b where B4:"h b = x"
+      using A3 by blast
+    have B5:"(h b) = (SOME a. (a \<in> A  \<and> ((f a = b)) )) \<or> (h b) =  a0"
+      using h_def by auto
+    show "x \<in> A"
+    proof(cases "h b = a0")
+      case True
+      then show ?thesis
+        by (simp add: B01 B4)
+    next
+      case False
+      have B6:"(h b) = (SOME a. (a \<in> A \<and> (f a = b)))"
+        using B5 False by auto
+      then show ?thesis
+        by (smt (verit, best) B4 False assms h_def someI_ex)
+    qed
+   qed
+  show ?thesis
+    using B2 B3 by blast
+qed
+
+lemma cardlt_obtains:
+  assumes "cardlt A B" obtains f where "inj_on f A \<and> (f`A) \<subseteq> B"
+  using assms by (meson cardlt_def)
+
+lemma cardgt_obtains:
+  assumes "A \<noteq> {}" and "cardgt A B" obtains f where "(f`A) = B"
+  using assms by (meson cardgt_def)
+
+lemma cardlt_imp_rev_cardgt:
+  assumes A0:"A \<noteq> {}" and A1:"cardlt A B"
+  shows "cardgt B A"
+proof-
+  obtain f where B0:"inj_on f A \<and> f`A \<subseteq>  B"
+    by (metis A1 cardlt_def) 
+  define finv where "finv=the_inv_into A f"
+  define a0 where  "a0= (SOME a. a \<in> A)"
+  define g where  "g=(\<lambda>y. (if y \<in> (f`A) then (finv y) else a0))"
+  have B1:"\<forall>a. a \<in> A \<longrightarrow> g (f (a)) = a"
+    by (simp add: B0 finv_def g_def the_inv_into_f_f)
+  have B2:"(g`B) \<subseteq> A"
+  proof
+    fix a assume A2:"a \<in> (g`B)"
+    obtain b where B3:"b \<in> B \<and> a=(g b)"
+      using A2 by blast
+    show "a \<in> A"
+      by (simp add: A0 B0 B3 a0_def finv_def g_def some_in_eq the_inv_into_into)
+  qed  
+  have B4:"A \<subseteq> g`B"
+    by (metis B0 B1 image_subset_iff subsetI)
+  show ?thesis
+    using B2 B4 cardgt_def by auto
+qed
+
+lemma cardgt_imp_rev_cardlt:
+  assumes A0:"B \<noteq> {}" and A1:"cardgt B A"
+  shows "cardlt A B"
+proof-
+  obtain g where B0:"(g`B)=A"
+    by (meson A0 A1 cardgt_obtains)
+  have B1:"\<forall>a. a \<in> A \<longrightarrow> (\<exists>b. b \<in> B \<and> g b = a) "
+    using B0 by blast
+  define b0 where "b0 = (SOME b. b \<in> B)"
+  define f where "f = (\<lambda>x. if x \<in> A then (SOME y.  y \<in> B \<and> g y = x) else b0)"
+  have B2:"inj_on f A"
+  proof-
+    have B4: "\<And>x1 x2. x1 \<in> A \<and> x2 \<in> A \<and> (f x1 = f x2) \<longrightarrow> x1 =x2"
+    proof
+      fix x1 x2 assume  A2:"x1 \<in> A \<and> x2 \<in> A \<and> (f x1 = f x2)"
+      obtain y1 y2 where B3:"y1 \<in> B \<and> g y1 = x1 \<and> y2 \<in> B \<and> g y2 = x2"
+        using A2 B1 by presburger
+      show "x1 = x2"
+        by (metis (mono_tags, lifting) A2 B3 f_def someI_ex)
+    qed
+    show ?thesis
+      by (simp add: B4 inj_onI)
+  qed
+  have B5:"f`A \<subseteq> B"
+  proof
+    fix b assume A3:"b \<in> f`A"
+    obtain a where B6:"a \<in> A \<and> b = f a"
+      using A3 by blast
+    show "b \<in> B"
+      by (metis (full_types, lifting) B1 B6 f_def someI_ex)
+  qed
+  show ?thesis
+    by (metis B2 B5 cardlt_def)
+qed
+  
+      
+
 definition is_countable :: "'a set \<Rightarrow> bool" where
   "is_countable I \<equiv> (\<exists>f::'a \<Rightarrow> nat. inj_on f I)"
 
+
 definition is_countably_infinite :: "'a set \<Rightarrow> bool" where
   "is_countably_infinite I \<equiv> is_countable I \<and> infinite I"
+
+definition even_odd_glue::"(nat \<Rightarrow> 'a) \<Rightarrow>  (nat \<Rightarrow> 'a) \<Rightarrow> (nat \<Rightarrow> 'a) " where
+  "even_odd_glue f g  \<equiv> (\<lambda>(n::nat). (if (even n) then f(n div 2) else g((n-1) div 2)))"
+
+lemma even_odd_glue_mem1:
+  fixes f g::"(nat \<Rightarrow> 'a)" 
+  assumes A0:"inj_on f (UNIV::nat set) \<and> inj_on g (UNIV::nat set)" and
+           A1:"h=even_odd_glue f g" and 
+           A2:"range f \<inter> range g = {}"
+  shows "inj_on h (UNIV::nat set)"
+proof-
+  have B0:"\<And>(n1::nat) n2. h n1 = h n2 \<longrightarrow> n1 = n2"
+  proof
+     fix n1 n2 assume A3:"h n1 = h n2"
+     show "n1=n2"
+       by (smt (verit) A0 A1 A2 A3 Suc_1 Zero_not_Suc bit_eq_rec disjoint_iff even_odd_glue_def 
+            injD nonzero_mult_div_cancel_left odd_two_times_div_two_nat range_eqI)
+  qed
+  show ?thesis
+    by (simp add: B0 injI)
+qed
+
+
+
+lemma is_countably_infinite_imp:
+  "is_countably_infinite I \<Longrightarrow> (is_countable I \<and> infinite I)" 
+  by (simp add: is_countably_infinite_def)
+
+lemma is_countably_infinite_if:
+  " (is_countable I \<and> infinite I) \<Longrightarrow> is_countably_infinite I"
+  by (simp add: is_countably_infinite_def)
 
 lemma countable_subset:
   "is_countable A \<Longrightarrow> B \<subseteq> A \<Longrightarrow> is_countable B"
   apply(simp add: is_countable_def)
   using inj_on_subset by blast
+
+lemma is_countable_imp_cardlt:
+  assumes "is_countable I"
+  shows "cardlt I (UNIV::nat set)"
+  apply( simp add: cardlt_def)
+  using assms is_countable_def by auto
+
+
+
+
+lemma infinite_ge_countably_infinite:
+  "infinite A \<longleftrightarrow> (cardlt (UNIV::nat set) A)" (is "?L \<longleftrightarrow> ?R")
+proof
+  assume L:?L show ?R
+    by (simp add: L cardlt_def infinite_countable_subset)
+  next
+  assume R:?R show ?L
+    by (meson R cardlt_def infinite_iff_countable_subset)
+qed
 
 lemma inj_countable_codom:
   "inj_on f A \<Longrightarrow> f`A=B \<Longrightarrow> is_countable B \<Longrightarrow> is_countable A"
@@ -291,115 +469,104 @@ lemma countably_infinite_obtain_bij_nat:
   assumes "is_countably_infinite A"
   obtains f where "bij_betw f A (UNIV::nat set)"
   using assms countably_infinite_bij_nat by blast
-(*
-(*scuffed*)
-lemma countably_infinite_disjoint_bsup:
-  "is_countably_infinite A  \<and> is_countably_infinite B \<and> (A \<inter> B = {}) \<Longrightarrow> is_countably_infinite (A \<union> B)"
+
+lemma countably_infinite_bij_countably_infinite:
+  assumes A0:"is_countably_infinite A" and A1:"bij_betw f A B"
+  shows "is_countably_infinite B"
+  by (metis A0 A1 bij_betw_finite bij_betw_imp_surj_on is_countably_infinite_def surj_countable_dom)
+
+
+lemma disjoint_emb:
+  fixes A B::"'a set" and u::"'b"
+  assumes A0:"bij_betw f A B"
+  shows "\<exists>g. bij_betw g A {b0. (\<exists>b \<in> B. b0=(u, b))}"
 proof-
-  assume A0:"is_countably_infinite A  \<and> is_countably_infinite B \<and>  (A \<inter> B = {})"
-  obtain f g where B0:"bij_betw f (UNIV::nat set) A" and B1:"bij_betw g (UNIV::nat set) B"
-    by (meson A0 bij_betw_inv countably_infinite_bij_nat)
-  define h::"nat \<Rightarrow> 'a" where  "h = (\<lambda>(n::nat). (if (even n) then f(n div 2) else g((n-1) div 2)))"
-  have B2:"\<And>n. h n \<in> A \<longrightarrow> even n"
+  define g where "g=(\<lambda>a. (u, f a))"
+  have B0:"\<And>(x1::'a) (x2::'a). (x1 \<in> A \<and> x2 \<in> A \<and>  g x1 = g x2) \<longrightarrow> x1 = x2"
   proof
-     fix n::"nat" assume A1:"h n \<in> A"
-     show "even n"
-    proof(rule ccontr)
-      assume A2:"\<not>(even n)"
-      have B20:"h n = g ((n - 1) div 2)"
-        using A2 h_def by presburger
-      have B21:"h n \<in> B"
-        using B1 B20 bij_betw_imp_surj_on by blast
-      show "False"
-        using A0 A1 B21 by blast
-    qed
+    fix x1 x2 assume A1:"(x1 \<in> A \<and> x2 \<in> A \<and>  g x1 = g x2)"
+    obtain a1 a2 where B1:"g x1 = (u, (f a1)) \<and> g x2 = (u, (f a2))"
+      by (simp add: g_def)
+    have B1:"f a1 = f a2"
+      using A1 B1 by auto
+    show "x1 =x2"
+      by (metis A1 Pair_inject assms bij_betw_inv_into_left g_def)
   qed
-  have B3:"inj_on h (UNIV::nat set)"
-  proof-
-    have B30:"\<And>(n1::nat) n2. h n1 = h n2 \<longrightarrow> n1 = n2"
-    proof
-      fix n1 n2 assume A3:"h n1 = h n2"
-      consider "((h n1) \<in> B \<and> (h n2) \<in> B) | ((h n1) \<in> A \<and> (h n2) \<in> A)"
-        by (metis A3 B0 B1 bij_betw_imp_surj_on h_def range_eqI)
-      show "n1 = n2"
-      proof(cases "((h n1) \<in> A \<and> (h n2) \<in> A)")
-        assume A4:"((h n1) \<in> A \<and> (h n2) \<in> A)"
-        have B04:"even n1 \<and> even n2"
-          by (simp add: A4 B2)
-        then show ?thesis
-          by (metis (full_types) A3 B0 UNIV_I bij_betw_iff_bijections bit_eq_rec h_def)
-      next
-        case False
-        have B05:"((h n1) \<in> B \<and> (h n2) \<in> B)"
-          using False \<open>\<And>thesis::bool. ((h::nat \<Rightarrow> 'a) (n1::nat) \<in> (B::'a set) \<and> h (n2::nat) \<in> B \<or> h n1 \<in> (A::'a set) \<and> h n2 \<in> A \<Longrightarrow> thesis) \<Longrightarrow> thesis\<close> by blast
-        have B06:"h n1 = g ((n1-1) div 2)"
-          by (metis A3 B0 False bij_betw_imp_surj_on h_def range_eqI)
-        have B07:"h n2 = g ((n2 -1) div 2)"
-          by (metis A3 B0 False bij_betw_imp_surj_on h_def range_eqI)
-        have B08:" g ((n1 -1) div 2) =  g ((n2 -1) div 2)"
-          using A3 B06 B07 by presburger
-        have B09:"(n1 - 1) div 2 = (n2 - 1) div 2"
-          by (smt (verit) B08 B1 UNIV_I bij_betw_iff_bijections)
-        then show ?thesis
-          by (metis A3 B0 False One_nat_def bij_betw_imp_surj_on dvd_mult_div_cancel dvd_triv_left h_def odd_Suc_minus_one odd_two_times_div_two_nat range_eqI)
-      qed
-   qed
-   show ?thesis
-     by (simp add: B30 injI)
-  qed
-  have B4:"(h`(UNIV::nat set)) = (A \<union> B)"
-  proof-
-    have B40:"\<And>y. y \<in> (A \<union> B) \<longrightarrow> (\<exists>(n::nat). h n = y)"
-    proof
-      fix y assume A5:"y \<in> A \<union> B"
-      show "\<exists>(n::nat). h n = y"
-      proof(cases "y \<in> A")
-        case True
-        obtain k::"nat" where "f k = y"
-          by (meson B0 True bij_betw_iff_bijections)
-        have "h (2 * k) = y"
-          by (simp add: \<open>(f::nat \<Rightarrow> 'a) (k::nat) = (y::'a)\<close> h_def)
-        then show ?thesis
-          by blast
-      next
-        case False
-        have "y \<in> B"
-          using A5 False by auto
-        obtain k::"nat" where "g k = y"
-          by (meson A5 B1 False Un_iff bij_betw_iff_bijections)
-        have "h (2 * k +1) = y"
-          by (simp add: \<open>(g::nat \<Rightarrow> 'a) (k::nat) = (y::'a)\<close> h_def)
-        then show ?thesis
-          by auto
-      qed
-    qed
-  have B41:"A \<union> B \<subseteq> h`(UNIV::nat set)"
-    using B40 by blast
-  have B42:"h`(UNIV::nat set) \<subseteq> A \<union> B"
-    by (metis B0 B1 UnI1 UnI2 bij_betw_imp_surj_on h_def image_subset_iff range_eqI)
+  have B1:"g ` A = {b0::'b \<times> 'a. \<exists>b::'a\<in>B. b0 = (u, b)}"
+    by (smt (verit) Collect_cong assms bij_betw_iff_bijections g_def image_def)
   show ?thesis
-    using B41 B42 by blast
-  qed
-  show "is_countably_infinite (A \<union> B)"
-    by (metis A0 B4 \<open>\<And>thesis::bool. (\<And>(f::nat \<Rightarrow> 'a) g::nat \<Rightarrow> 'a. \<lbrakk>bij_betw f UNIV (A::'a set); bij_betw g UNIV (B::'a set)\<rbrakk> \<Longrightarrow> thesis) \<Longrightarrow> thesis\<close> bij_betw_def infinite_Un inj_countable_codom is_countably_infinite_def surj_countable_dom)
+  apply(auto simp add: bij_betw_def inj_on_def)
+    using B0 B1 by blast
 qed
+
+lemma disjoint_emb_def:
+  fixes A B::"'a set" and u::"'b" and f::"'a \<Rightarrow> 'a"
+  defines "g \<equiv> (\<lambda>a. (u, f a))"
+  assumes A0:"bij_betw f A B"
+  shows "bij_betw g A {b0. (\<exists>b \<in> B. b0=(u, b))}"
+proof-
+  have B0:"\<And>(x1::'a) (x2::'a). (x1 \<in> A \<and> x2 \<in> A \<and>  g x1 = g x2) \<longrightarrow> x1 = x2"
+  proof
+    fix x1 x2 assume A1:"(x1 \<in> A \<and> x2 \<in> A \<and>  g x1 = g x2)"
+    obtain a1 a2 where B1:"g x1 = (u, (f a1)) \<and> g x2 = (u, (f a2))"
+      by (simp add: g_def)
+    have B1:"f a1 = f a2"
+      using A1 B1 by auto
+    show "x1 =x2"
+      by (metis A1 Pair_inject assms bij_betw_inv_into_left g_def)
+  qed
+  have B1:"g ` A = {b0::'b \<times> 'a. \<exists>b::'a\<in>B. b0 = (u, b)}"
+    by (smt (verit) Collect_cong assms bij_betw_iff_bijections g_def image_def)
+  show ?thesis
+  apply(auto simp add: bij_betw_def inj_on_def)
+    using B0 apply auto[1]
+    using A0 bij_betwE g_def apply blast
+    using B1 by blast
+qed
+
+
 
 lemma infinite_iso:
   "infinite A \<Longrightarrow> A \<subseteq> B \<Longrightarrow> infinite B"
   using finite_subset by blast
 
-lemma countably_infinite_bsup:
-  assumes "is_countably_infinite A  \<and> is_countably_infinite B"  
+lemma is_countable_imp_countable:
+  "is_countable A \<Longrightarrow> countable A"
+  by (simp add: countable_def is_countable_def)
+
+lemma is_countable_if_countable:
+  "countable A \<Longrightarrow> is_countable A"
+  by (simp add: countable_def is_countable_def)
+
+lemma infinite_imp_infinite_union:
+  "infinite A \<Longrightarrow> infinite B \<Longrightarrow> infinite (A \<union> B)"
+  by simp
+
+lemma cinfinite_union:
+  assumes "is_countably_infinite A \<and> is_countably_infinite B"
   shows "is_countably_infinite (A \<union> B)"
 proof-
-  have B0:"infinite (A \<union> B)"
+  have "infinite (A \<union> B)"
     using assms is_countably_infinite_def by auto
-  define X where "X = (A \<union> B) -B"
-  have "X \<union> B = A \<union> B"
-    by (simp add: X_def)
-  have "X \<inter> B = {}"
-    by (simp add: Int_commute X_def)
-  have 
-  have B1:"is_countable (A \<union> B)"
-*)
+  have "is_countable A \<and> is_countable B"
+    by (simp add: assms is_countably_infinite_imp)
+  have "countable A \<and> countable B"
+    by (simp add: \<open>is_countable (A::'a set) \<and> is_countable (B::'a set)\<close> is_countable_imp_countable)
+  have "countable (A \<union> B)"
+    by (simp add: \<open>countable (A::'a set) \<and> countable (B::'a set)\<close>)
+  have "is_countable (A \<union> B)"
+    using \<open>countable ((A::'a set) \<union> (B::'a set))\<close> is_countable_if_countable by blast
+  have "is_countably_infinite (A \<union> B)"
+    using \<open>infinite ((A::'a set) \<union> (B::'a set))\<close> \<open>is_countable ((A::'a set) \<union> (B::'a set))\<close> is_countably_infinite_def by blast
+  show ?thesis
+    by (simp add: \<open>is_countably_infinite ((A::'a set) \<union> (B::'a set))\<close>)
+qed
+
+lemma empty_countable:
+  "is_countable {}"
+  by (simp add: is_countable_def)
+
+
+
+
 end
