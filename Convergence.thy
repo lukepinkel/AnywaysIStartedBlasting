7fea2f1dcd8d97f77135e9b9f1778f10b50ae69a
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2024-04-09 15:16:29 -0700

    Update Convergence.thy

--- Convergence.thy
+++ Convergence.thy
@@ -40,6 +40,22 @@ lemma upclosedE:
   assumes up:"upclosed X \<A>" shows upcl_sub:"(\<A> \<subseteq> Pow X)" and upcl:"(\<And>A B. \<lbrakk>A \<in> \<A>; B \<in> Pow X; A \<subseteq> B\<rbrakk> \<Longrightarrow> B \<in> \<A>)"
   using up upclosed_def apply blast
   by (meson up upclosed_def)
+
+lemma upcl_memI:
+  "\<lbrakk>E \<in> Pow X; (\<exists>A \<in> \<A>. A \<subseteq> E)\<rbrakk> \<Longrightarrow>  E \<in> upcl X \<A>"
+  by (simp add: upcl_def)
+
+lemma upcl_memE1:
+   " E \<in> upcl X \<A> \<Longrightarrow> E \<in> Pow X"
+   by (simp add: upcl_def) 
+
+lemma upcl_memE2:
+   "E \<in> upcl X \<A> \<Longrightarrow> (\<exists>A \<in> \<A>. A \<subseteq> E)"
+   by (simp add: upcl_def) 
+
+lemma upcl_mem_iff:
+  "E \<in> upcl X \<A> \<longleftrightarrow> E \<in> Pow X \<and> (\<exists>A \<in> \<A>. A \<subseteq> E)"
+  by (simp add: upcl_def)
   
 lemma upcl_closed[simp]:
   "(upcl X \<A>) \<subseteq> (Pow X)"
@@ -131,6 +147,12 @@ definition fil::"'a set \<Rightarrow> 'a set set \<Rightarrow> bool" where
 definition pfil::"'a set \<Rightarrow> 'a set set \<Rightarrow> bool" where
   "pfil X \<F> \<equiv>  \<F>  \<noteq> {} \<and>  \<F> \<subseteq> Pow X \<and> (\<forall>A F. A \<in> Pow X \<and> F \<in> \<F> \<and> F \<subseteq> A \<longrightarrow> A \<in> \<F>) \<and> (\<forall>F1 F2. F1 \<in> \<F>  \<and>  F2 \<in> \<F> \<longrightarrow> F1 \<inter> F2 \<in> \<F> ) \<and>  \<F> \<noteq> Pow X"
 
+abbreviation filters_on where
+  "filters_on X \<equiv> {\<F>. fil X \<F>}"
+
+abbreviation pfilters_on where
+  "pfilters_on X \<equiv> {\<F>. pfil X \<F>}"
+
 lemma pfil_fil:
   "pfil X \<F> \<Longrightarrow> fil X \<F>"
   by (simp add: fil_def pfil_def)
@@ -152,6 +174,7 @@ lemma filE:
  using f fil_def apply blast
  by (metis PowD Pow_top ex_in_conv f fil_def subsetD)
 
+
 lemma pfilE:
   assumes f:"pfil X \<F>" 
   shows pfilsub:"\<F> \<subseteq> Pow X" and 
@@ -164,13 +187,22 @@ lemma pfilE:
         using f filupc pfil_fil apply blast
         using f filinc pfil_fil apply blast
         using f pfil_def apply blast
-        apply (simp add: f filtop pfil_fil)
+        apply (simp add: f filtop  pfil_fil)
         by (metis empty_subsetI f pfil_def subsetI subset_antisym)
+
+lemma fil_upcl:
+  "fil X \<F> \<Longrightarrow> upclosed X \<F>"
+  apply(rule upclosedI,simp add: filsub)
+  using filupc by blast
+
+lemma pfil_upcl:
+  "pfil X \<F> \<Longrightarrow> upclosed X \<F>"
+  by (simp add: fil_upcl pfil_fil)
         
 lemma filter_inter:
   assumes ef:"\<forall>F. F \<in> EF \<longrightarrow> fil X F" and ne:"EF \<noteq> {}"
   shows "fil X (\<Inter>EF)"
-  apply(auto simp add: fil_def)
+  apply(auto simp add:fil_def)
   apply (metis InterI ef empty_iff filtop)
   apply (metis IntD2 Int_commute Pow_iff all_not_in_conv ef fil_def inf.absorb_iff2 ne)
   apply (meson PowI ef filupc)
@@ -236,22 +268,82 @@ lemma grill_anti:
   shows "grill X \<B> \<subseteq> grill X \<A>"
   by (meson grill_memI grillmesh1 grillsub in_mono sub1 subsetI)
 
-lemma grill_mem_upcl:
+lemma grill_mem_upcl1:
   assumes sub1:"\<A> \<subseteq> Pow X" and sub2:"E \<in> Pow X" and up:"upclosed X \<A>" 
-  shows "E \<in> grill X \<A> \<longleftrightarrow> (X-E) \<notin> \<A>"
-  by (metis (no_types, lifting) Diff_Diff_Int Diff_subset PowD PowI grill_mesh inf.absorb2 mem_Collect_eq mesh_props1 sub1 sub2 up)
-
-lemma grill_upcl:
-  assumes sub1:"\<A> \<subseteq> Pow X "
-  shows grillup:"upclosed X (grill X \<A>)" and "grill X (grill X \<A>) = upcl X \<A>"
+  shows "E \<in> \<A> \<longleftrightarrow> (X-E) \<notin> grill X \<A>" 
 proof-
-  show "upclosed X (grill X \<A>)" 
-    apply(rule upclosedI)
-    apply (simp add: grill_sub)
-    by (metis (no_types, lifting) CollectD CollectI grill_mesh mesh_sub sub1)
-  show "grill X (grill X \<A>) = upcl X \<A>" 
-  proof
-    show "grill X (grill X \<A>) \<subseteq>  upcl X \<A> "
+  have " (X-E) \<notin> grill X \<A> \<longleftrightarrow> \<not>(\<forall>A \<in> \<A>. A \<inter> (X-E) \<noteq> {})" by (meson Diff_subset Pow_iff grill_memI grillmesh1) 
+  also have "...           \<longleftrightarrow> (\<exists>A \<in> \<A>. A \<inter> (X-E) = {})" by simp
+  also have "...           \<longleftrightarrow> (\<exists>A \<in> \<A>. A \<subseteq> E)"  using Diff_triv sub1 subset_iff by fastforce
+  then have "(X-E) \<notin> grill X \<A>  \<longleftrightarrow> (\<exists>A \<in> \<A>. A \<subseteq> E)"   by (simp add: \<open>(X - E \<notin> grill X \<A>) = (\<not> (\<forall>A\<in>\<A>. A \<inter> (X - E) \<noteq> {}))\<close>)
+  then have "(X-E) \<notin> grill X \<A> \<Longrightarrow> E \<in> \<A>"  using sub2 up upcl by blast
+  then have "E \<in> \<A> \<Longrightarrow> (X-E) \<notin> grill X \<A>"   using \<open>(X - E \<notin> grill X \<A>) = (\<exists>A\<in>\<A>. A \<subseteq> E)\<close> by blast
+  then show ?thesis
+  using \<open>X - E \<notin> grill X \<A> \<Longrightarrow> E \<in> \<A>\<close> by blast
+qed
+
+
+lemma grill_mem_upcl2:
+  assumes sub1:"\<A> \<subseteq> Pow X" and sub2:"E \<in> Pow X" and up:"upclosed X \<A>" 
+  shows "(X-E) \<notin> \<A> \<longleftrightarrow> E \<in> grill X \<A>"
+  using grill_mem_upcl1
+  by (metis Diff_Diff_Int Diff_subset Pow_iff inf.absorb2 sub1 sub2 up)
+ 
+lemma grill_mesh2:
+  assumes sub1:"\<A> \<subseteq> Pow X" and up:"upclosed X \<A>" 
+  shows "grill X \<A> = {E \<in> Pow X. (X-E) \<notin> \<A>}"      
+  apply(simp add:grill_def)   
+  using assms mem_Collect_eq apply(auto)
+  by (simp add: grill_mem_upcl2 grillmesh1) 
+
+lemma mesh_iff_gril:
+  assumes sub1:"\<A> \<subseteq> Pow X "  and sub2:" \<B> \<subseteq> Pow X "
+  shows "\<A>#\<B> \<longleftrightarrow> \<A> \<subseteq> grill X \<B>"
+  apply(auto simp add:grill_def mesh_def subset_eq)
+  using sub1 by(auto)
+
+lemma grill_upcl1:
+  assumes sub1:"\<A> \<subseteq> Pow X "
+  shows grillup:"upclosed X (grill X \<A>)" 
+  apply(rule upclosedI)
+  apply (simp add: grill_sub)
+  by (metis (no_types, lifting) CollectD CollectI grill_mesh mesh_sub sub1)
+
+
+lemma grill_upcl2:
+  assumes sub1:"\<A> \<subseteq> Pow X " and sub2:"E \<in> Pow X"
+  shows "E \<in> grill X (grill X \<A>)  \<longleftrightarrow> E \<in>  upcl X \<A>"
+proof-
+  have "E \<in>  grill X (grill X \<A>) \<longleftrightarrow> (X-E) \<notin>  (grill X \<A>)"   by (meson grill_mem_upcl2 grill_sub grill_upcl1 sub1 sub2)
+  also have "...  \<longleftrightarrow> \<not> (\<forall>A \<in> \<A>. A \<inter> (X-E) \<noteq> {})" by (meson Diff_subset PowI grill_memI grillmesh1)
+  also have "...  \<longleftrightarrow> (\<exists>A \<in> \<A>. A \<inter> (X-E) = {}) " by simp
+  also have "...  \<longleftrightarrow>  (\<exists>A \<in> \<A>. A \<subseteq> E)" by (metis Diff_eq_empty_iff Int_Diff PowD inf.order_iff sub1 subset_iff)
+  also have "... \<longleftrightarrow>  E \<in>  upcl X \<A>" using  upcl_mem_iff sub2 by blast
+  then show ?thesis
+  using calculation by blast
+qed
+
+lemma refined_pfil_mesh:
+  assumes pf1:"pfil X \<F>" and pf2:" pfil X \<G>" and pr:" \<F> \<preceq> \<G> "
+  shows  "\<F>#\<G> "
+proof(rule ccontr)
+  assume  "\<not> \<F>#\<G> " then obtain F G where f1:"F \<in> \<F>" and g1:"G \<in> \<G>" and fg:"F \<inter> G = {}" using mesh_def by blast
+  obtain "\<F> \<subseteq> Pow X" and  "\<G> \<subseteq> Pow X" and "upclosed X \<G>" using pf1 pf2 by (simp add: pfil_upcl pfilsub) 
+  then have "\<F> \<subseteq> \<G>" using pr preceq_of_iso by metis
+  then obtain "F \<in> \<G>"  using f1 by blast
+  then have "{} \<in>  \<G>"  by (metis fg g1 pf2 pfilinc)
+  then show False  using pf2 pfilnbt by auto
+qed
+
+
+locale conv=
+  fixes X::"'a set" and q::"('a set set \<times> 'a) set"
+
+locale convergence=conv+
+  assumes frel:"q \<subseteq> (pfilters_on X) \<times> X"
+
+locale iso_conv=conv+
+  assumes iso:"\<And>x F G. \<lbrakk>(F, x) \<in> q; F \<preceq> G\<rbrakk> \<Longrightarrow> (\<G>, x) \<in> q "
 
 
 end
